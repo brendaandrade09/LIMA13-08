90436d33b32cd3cce8fd3d8045d941272a157f37
Brenda <brenda.andrade@edu.senai.br>
2026-09-24 14:26:38 -0300

    é

--- projeto03_css/js/login.js
+++ projeto03_css/js/login.js
@@ -1,3 +1,18 @@
 let login = document.getElementById("txtLogin");
 let senha = document.getElementById("txtSenha");
 let botao = document.getElementById("btnClicar");
+let resposta = document.getElementById("resp");
+
+const loginBd = "email@gmail.com";
+const senhaBd = "12345";
+
+function clicar() {
+    let usuarioDigitado = login.value
+    let senhaDigitada = senha.value
+    if(senhaDigitada === senhaBd && usuarioDigitado === loginBd){
+ window.location.href='../../index.html';   
+}else{
+    alert(`Login ou senha incorretos!!!`)
+}
+}
+botao.addEventListener("click", clicar)
